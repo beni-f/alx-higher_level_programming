f2636f37cf65c97d700f4edff686b4c51f335a1e
beni-f <benifissha@gmail.com>
2023-09-06 12:59:58 +0300

    updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphabet in range(97, 122):
     if alphabet != 113 and alphabet != 101:
-        print(chr(alphabet), end = "")
+        print("{}".format(chr(alphabet)), end = "")
