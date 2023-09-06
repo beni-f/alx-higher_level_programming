6775247debdfe8991fb3407bb7783c5a5857e35c
beni-f <benifissha@gmail.com>
2023-09-06 13:31:53 +0300

    updated

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    if (num < 10):
-        zero = 0
-        print(f"{zero}{num},", end =" ")
-    else:
-        print(f"{num}", end = "")
-        if num != 99:
-            print(", ", end = "")
-        else:
-            print("")
+    print("{},".format(num), end = " ")
+        
+        
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for num1 in range(0, 9):
     for num2 in range(0, 10):
         if num1 < num2:
-            print(f"{num1}{num2}", end = "")
+            print("{}{}".format(num1, num2), end = "")
             if num1 == 8 and num2 == 9:
                 print("")
             else:
