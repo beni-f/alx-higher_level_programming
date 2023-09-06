6364c4828c7d4a67d538ed05408d1b2ed35c3de3
beni-f <benifissha@gmail.com>
2023-09-06 15:09:47 +0300

    updated

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
     prod = 1
-    for i in range(b):
-        prod *= a
+    if b > 0:
+        for i in range(b):
+                prod *= a
+    else:
+        b *= -1
+        for i in range(b):
+            prod *= 1 / a
     return prod
