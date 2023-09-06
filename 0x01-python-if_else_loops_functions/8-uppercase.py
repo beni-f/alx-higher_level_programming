7b7d4cb2c30b23031014a508bbadee09d7f27759
beni-f <benifissha@gmail.com>
2023-09-06 16:01:44 +0300

    updated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if str[i] >= chr(97) and str[i] <= chr(122):
-            str[i] = int(str[i]) + 32 
-    
-    print(chr(str))
-
-uppercase("beni")
-
+    for ch in str:
+        if ord(ch) >= ord('a') and ord(ch) <= ord('z'):
+            c = chr(ord(ch) - 32)
+        else:
+            c = ch
+        print("{}".format(c), end="")
+    print("")
