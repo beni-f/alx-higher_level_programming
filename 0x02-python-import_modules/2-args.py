513b6bbbc9d9129731ef34235a5822b1b1633510
beni-f <benifissha@gmail.com>
2023-09-07 15:47:37 +0300

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 0:
+    if len(sys.argv) - 1 == 0:
         print("{} arguments.".format(len(sys.argv) - 1))
-    elif len(sys.argv) > 0:
+    else:
         print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(len(sys.argv)):
         if i > 0:
