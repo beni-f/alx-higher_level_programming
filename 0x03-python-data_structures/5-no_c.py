28ff79f0a32488873ff4829209fe346ceae73c28
beni-f <benifissha@gmail.com>
2023-09-09 12:41:19 +0300

    updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 def no_c(my_string):
+    new_string = ''
     for letter in my_string:
-        if letter == 'c' or letter == 'C':
-            letter = 'a'
-    return my_string
-    
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+        if letter != 'c' and letter != 'C':
+            new_string += letter
+        else:
+            letter = ""
+            new_string += letter
+    return new_string
