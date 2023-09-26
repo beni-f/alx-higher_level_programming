dd39f1f214363b3ce57fbbf2ddad1fc8b93d9ffe
beni-f <benifissha@gmail.com>
2023-09-26 20:38:57 +0300

    updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Define a class Square"""
 class Square:
+    """ defines a function called __init__ """
     def __init__(self, size):
+        """ initializes __size of self with size """
         self.__size = size
