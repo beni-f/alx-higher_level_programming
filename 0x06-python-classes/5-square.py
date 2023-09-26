ae3529345c7844201648fbbcb7ad2f3ba5d9d64d
beni-f <benifissha@gmail.com>
2023-09-27 00:37:33 +0300

    intial commit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,13 +2,23 @@
 """ Create a class called square """
 class Square:
     """ define function __init__ """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0,0)):
         self.__size = size
+        self.__position = position
     """ getter """
+    @property
+    def position(self):
+        return self.__position
     @property
     def size(self):
         return self.__size
     """ setter """
+    @position.setter
+    def position(self, value):
+        if len(self.__position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -28,5 +38,20 @@ class Square:
             print()
         for i in range(self.__size):
             for j in range(self.__size):
-                print("#", end="")
-            print()
+                 print("".join([" " for k in range(self.__position[0])]), end="")
+            print("".join(["#" for z in range(self.__size)]))
+    
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
