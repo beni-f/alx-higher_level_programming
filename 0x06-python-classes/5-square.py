c74bc9afdef5ba5103d968b6178e3d8f13522010
beni-f <benifissha@gmail.com>
2023-09-26 22:24:30 +0300

    inital commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Create a class called square """
+class Square:
+    """ define function __init__ """
+    def __init__(self, size=0):
+        self.__size = size
+    """ getter """
+    @property
+    def size(self):
+        return self.__size
+    """ setter """
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            """ raise error """
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            """ raise error """
+            raise ValueError("size must be >= 0")
+        else:
+            """ set size to value """
+            self.__size = value
+    def area(self):
+        """ return the area """
+        return self.__size ** 2
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
