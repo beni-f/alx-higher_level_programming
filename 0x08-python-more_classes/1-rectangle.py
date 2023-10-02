4181575723b5b6ff14ea7a6a13c7c700da60f85b
beni-f <benifissha@gmail.com>
2023-10-02 21:37:47 +0300

    intial commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+    Create class Rectangle
+"""
 class Rectangle:
+    """Rectangle"""
     def __init__(self, width=0, height=0):
         """initalize height and width"""
         self.__width = width
