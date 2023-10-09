69bebe818139ba1612bb77b450333cbc48248bb8
beni-f <benifissha@gmail.com>
2023-10-10 01:07:04 +0300

    inital commit

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Raises and exception"""
 class BaseGeometry:
+    """raise exception with a message"""
     def area(self):
-        """raise exception with a message"""
         raise Exception("area() is not implemented")
