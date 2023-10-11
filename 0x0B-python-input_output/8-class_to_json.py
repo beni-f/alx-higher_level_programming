7a6838175fe1bd8a1129d03bf9437799ab320fbe
beni-f <benifissha@gmail.com>
2023-10-11 16:40:42 +0300

    initial commit

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,11 +2,11 @@
 """
 8-class_to_json.py
 """
-import json
+
 def class_to_json(obj):
     """
     class_to_json - returns the dictionary description for JSON
     arg:
         obj - class object
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
