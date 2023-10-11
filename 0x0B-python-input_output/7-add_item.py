e6629c9247ca7153b85647b2daf9c9ffdd3d8924
beni-f <benifissha@gmail.com>
2023-10-11 16:22:46 +0300

    updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""
-7-add_item module
-"""
+
 import sys
-import json
-import os.path
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file = "add_item.json"
-json_list = []
-
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
-
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
-
-save_to_json_file(json_list, file)
+if __name__ == "__main__":
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except:
+        my_list = []
+    my_list += sys.argv[1:]
+    save_to_json_file(my_list, "add_item.json")
