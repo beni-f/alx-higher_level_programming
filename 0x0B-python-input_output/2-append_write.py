fdaf3cdc9d690095c4643a20f1eb21c41c869507
beni-f <benifissha@gmail.com>
2023-10-11 12:54:41 +0300

    inital commit

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def append_write(filename="", text=""):
+    """
+    append_write - appends a string to the end of a text file (UTF8),
+                and returns the number of characters written:
+    Args:
+        filename: name of the file
+        text: text to be written
+    Return: number of bytes written.
+    """
+    with open(filename, 'a',  encoding="UTF-8") as f:
+        return f.write(text)
