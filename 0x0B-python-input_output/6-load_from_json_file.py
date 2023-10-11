995f16c4cfbe02e4f3546f102fcff820140c5d33
beni-f <benifissha@gmail.com>
2023-10-11 15:10:28 +0300

    inital commit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-
+"""
+6-load_from_json_file.py
+"""
 import json
 def load_from_json_file(filename):
+    """
+    load_from_json_file - loads an object from JSON file.
+    Args:
+        filename: name of the file
+    """
     with open(filename , 'r') as f:
         return json.load(f)
