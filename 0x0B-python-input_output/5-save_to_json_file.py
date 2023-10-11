588da785741164ef610fc800ae4ec130887da733
beni-f <benifissha@gmail.com>
2023-10-11 13:30:40 +0300

    inital commit

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,5 +2,13 @@
 
 import json
 def save_to_json_file(my_obj, filename):
+    """
+        save_to_json_file - writes an Object to a text file,
+                        using a JSON representation:
+    Args:
+        my_obj: object
+        filename: name of the file
+    Return:
+    """
     with open(filename, 'w', encoding="UTF-8") as f:
         f.write(json.dumps(my_obj))
