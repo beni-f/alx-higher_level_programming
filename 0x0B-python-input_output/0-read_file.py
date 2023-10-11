9243059713e104ec937fe8e37cba3f5121666fed
beni-f <benifissha@gmail.com>
2023-10-11 12:41:00 +0300

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,8 +6,9 @@ def read_file(filename=""):
     """
         filename - the name of the file to be read
     """
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding="UTF-8") as f:
         """
             Opens and reads the file
         """
-        print(f.read())
+        for line in f:
+            print(line, end="")
