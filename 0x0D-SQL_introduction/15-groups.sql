3669b68131bfc09561b53251c2c73ecc5b0e3f1e
Beni Fissha <benifissha@gmail.com>
2023-11-14 22:40:06 +0300

    initial commit

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- a script that lists the number of records with the same score in the table second_table of the database 
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY number desc;
