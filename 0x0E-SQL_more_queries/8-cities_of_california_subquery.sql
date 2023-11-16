1ef69e1e2088e532b0ebdc86a6548fcb539fb7b4
Beni Fissha <benifissha@gmail.com>
2023-11-16 12:57:45 +0300

    initial commit

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- lists all the cities of california that can be found in the database hbtn_0d_usa
+-- lists all cities
+SELECT id, name
+FROM cities WHERE state_id = (
+        SELECT id FROM states WHERE name = 'California'
+) ORDER BY id ASC;
