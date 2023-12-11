74a2144b17284529b1c5928211cc89bbd628e4ad
Beni Fissha <benifissha@gmail.com>
2023-12-11 20:59:36 +0300

    updated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2] == NaN)
-{ 
-    console.log('No argument'); 
-} 
-else 
-{ 
-    console.log(process.argv[2]); 
+if (isNaN(process.argv[2])) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
