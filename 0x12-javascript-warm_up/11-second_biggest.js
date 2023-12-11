1d336e63cca7cb04a4ecf010bc37b7a85f03cda4
Beni Fissha <benifissha@gmail.com>
2023-12-11 20:43:41 +0300

    initial commit

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+if (process.argv[2] == null)
+    console.log(0);
+else
+{
+    let max = 0, secondMax = 0;
+    for (let i = 2; i < process.argv.length; i++)
+    {
+        if (process.argv[i] >= max)
+        {
+            secondMax = max;
+            max = process.argv[i];
+        }
+    }
+    console.log(secondMax);
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+let x = process.argv[2];
+for (let i = 0; i < x; i++)
+    console.log('C is fun');
