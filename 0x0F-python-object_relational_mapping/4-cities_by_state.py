b3c483eec8fed0e3d0a784c64d2418deac6ab51d
beni-f <benifissha@gmail.com>
2023-12-17 13:15:15 +0300

    initial commitg

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# A script that lists all cities from the database hbtn_0e_4_usa
+"""
+    4-cities_by_state.py
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    """
+        Code
+    """
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name`\
+                 FROM `cities` as `c`\
+                INNER JOIN `states` as `s` \
+                ON `c`.`state_id` = `s`.`id`\
+                ")
+
+    for cities in cur.fetchall():
+        print(cities)
