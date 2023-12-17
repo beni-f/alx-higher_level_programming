23db2968473ac4abeb0a9f214b06df95d71ea0d7
beni-f <benifissha@gmail.com>
2023-12-17 12:34:43 +0300

    updated

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# A script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
+"""
+    2-my_filter_states.py
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    """Code"""
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * \
+                 FROM `states` \
+                WHERE `name` = '{}'".format(sys.argv[4]))
+
+    for states in cur.fetchall():
+        print(states)
