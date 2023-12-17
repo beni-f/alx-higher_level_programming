aec9225f623a3837ecf0aafd59ea410182649a80
beni-f <benifissha@gmail.com>
2023-12-17 12:13:18 +0300

    initial commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 # A script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+
+"""
+    1-filter_states.py
+"""
 import sys
 import MySQLdb
 
