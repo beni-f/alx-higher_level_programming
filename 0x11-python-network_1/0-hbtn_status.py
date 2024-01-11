a10f8b43ca1722b4b67fff52254afd6a599939bc
beni-f <benifissha@gmail.com>
2024-01-11 14:49:28 +0300

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-    What's my status? #0
+	What's my status? #0
 """
 import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as url:
-    url_read = url.read()
-    req = "Body response:$\n\t- type: {}$\n\t- content: {}$\n\t- utf8 content: {}$"
-    print(req.format(type(url_read), url_read, url_read.decode('utf-8')))
+	url_read = url.read()
+	req = "Body response:$\n\t- type: {}$\n\t- content: {}$\n\t- utf8 content: {}$"
+	print(req.format(type(url_read), url_read, url_read.decode('utf-8')))
