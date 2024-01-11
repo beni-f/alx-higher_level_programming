e001faad17308586ea4e6319d07de946487aec32
beni-f <benifissha@gmail.com>
2024-01-11 15:50:58 +0300

    initial commit

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    POST an email #1
+"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    value = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=value)
+    print('Your email is: {}'.format(sys.argv[2]))
