ebc8a6e87e310f8f6bb9f72c44b3425a5ce443e7
beni-f <benifissha@gmail.com>
2024-01-11 14:44:32 +0300

    initial commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,6 @@
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as url:
+    url_read = url.read()
+    req = "Body response:$\n\t- type: {}$\n\t- content: {}$\n\t- utf8 content: {}$"
+    print(req.format(type(url_read), url_read, url_read.decode('utf-8')))
