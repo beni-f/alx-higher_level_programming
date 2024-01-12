4e2ff495a5d4b431bdff53a62700a3b407b8d317
beni-f <benifissha@gmail.com>
2024-01-12 12:47:29 +0300

    initial commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Search API
+"""
+import requests 
+import sys
+
+if __name__ == '__main__':
+    letter = ""
+    if sys.argv == 2:
+        letter = sys.argv[1]
+    value = {'q': letter}
+    url = requests.post('https://www.google.com')
+
+    try: 
+        resp = url.json()
+        if resp == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(resp['id'], resp['name']))
+    except ValueError:
+        print("Not a valid JSON")
+
+
+    
